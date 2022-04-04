5b98acd4e4b67efc63e80d39be366f65ce3d6bd1
cyberxsboy <cyberxsboy@163.com>
2022-04-05 02:23:55 +0800

    Update onekey_install_xray_pc.sh

--- onekey_install_xray_pc.sh
+++ onekey_install_xray_pc.sh
@@ -53,8 +53,8 @@ install_pc(){
     mv proxypool-linux-amd64-v0.6.0 proxypool
     chmod 755 proxypool
 
-    wget https://raw.githubusercontent.com/lanhebe/proxypool/master/config.yaml
-    wget https://raw.githubusercontent.com/lanhebe/proxypool/master/source.yaml
+    wget https://raw.githubusercontent.com/cyberxsboy/proxypool-xary/master/config/config.yaml
+    wget https://raw.githubusercontent.com/cyberxsboy/proxypool-xary/master/config/source.yaml
    
     cat > ./config.yaml <<-EOF
     domain: $your_domain
